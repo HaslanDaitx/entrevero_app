1384e89c9e17855cafcb433dffa5018e03905ad6
HaslanDaitx <164122728+HaslanDaitx@users.noreply.github.com>
2026-05-24 20:06:53 -0300

    Colocando confirmação de exclusão de participante

--- src/components/CardPessoa.jsx
+++ src/components/CardPessoa.jsx
@@ -17,7 +17,13 @@ function CardPessoa({
         type="button"
         onClick={(event) => {
           event.stopPropagation()
-          onRemoverParticipante(pessoa.id)
+
+           const confirmar = window.confirm(
+              `Deseja realmente excluir ${pessoa.nome}?`
+            )
+            if (confirmar) {
+              onRemoverParticipante(pessoa.id)
+            }
         }}
         className="absolute top-4 right-4 p-2 rounded-xl text-red-500 hover:bg-red-50 hover:text-red-700 transition"
       >
